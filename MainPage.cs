1aa6ecb78316b475a16aa110785b6fd0532bd4a8
ortzii <1000663187@hityash.org.il>
2026-01-22 08:55:59 +0200

    Add project files.

---
+++ MainPage.cs
@@ -0,0 +1,139 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Big17DataFirebase2.BusinessLogic;
+using Big17DataFirebase2.Model;
+using Big17DataFirebase2.Service;
+using Firebase.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Big17DataFirebase2
+{
+    [Activity(Label = "MainPage")]
+    public class MainPage : Activity
+    {
+        TextView tvusername, tvisadmin, tvuserslist;
+        Button btnRemoveUser;
+        Dialog mProgressDialog;
+        List<User> users;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.mainpage_layout);
+
+            InitializeViews();
+        }
+
+        private void InitializeViews()
+        {
+            tvusername = FindViewById<TextView>(Resource.Id.tvUsername);
+            tvisadmin = FindViewById<TextView>(Resource.Id.tvIsAdmin);
+            tvuserslist = FindViewById<TextView>(Resource.Id.tvUserslist);
+            btnRemoveUser = FindViewById<Button>(Resource.Id.btnUserRemove);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            tvusername.Text = ProManager.CurrentUser.FirstName;
+
+            if (ProManager.CurrentUser.IsAdmin)
+                tvisadmin.Visibility = ViewStates.Visible;
+
+            ShowProgressBar(true);
+            //GetUsersFromDB();
+            FetchUsersFromDB();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            FireBaseHelper.FirestoreEventListener = null;
+        }
+
+        private void FetchUsersFromDB()
+        {
+            FireBaseHelper.FetchUsersListener();
+            FireBaseHelper.FirestoreEventListener.getEvent += (error, args) =>
+            {
+                ShowProgressBar(false);
+                if (users != null)
+                    users.Clear(); //Clear users list
+                else
+                    users = new List<User>();
+
+                var snapshot = (QuerySnapshot)args.Result;
+                if (!snapshot.IsEmpty)
+                {
+                    var documents = snapshot.Documents;
+                    foreach (DocumentSnapshot item in documents)
+                    {
+                        User _user = new User()
+                        {
+                            Id = item.Id,
+                            FirstName = item.Get("FirstName").ToString(),
+                            LastName = item.Get("LastName").ToString(),
+                            UserEmail = item.Get("UserEmail").ToString(),
+                            UserMobile = item.Get("UserMobile").ToString(),
+                            UserPass = item.Get("UserPassword").ToString(),
+                            IsAdmin = bool.Parse(item.Get("IsAdmin").ToString())
+                        };
+                        users.Add(_user);
+                    }
+                    FillUsersList();
+                }
+            };
+        }
+
+        private async void GetUsersFromDB()
+        {            
+            users = await FireBaseHelper.GetUsersCollection();
+            if(users.Count > 0)
+            {
+                FillUsersList();
+            }
+            else
+            {
+                tvuserslist.Text = "Users collection is empty...";
+            }
+            ShowProgressBar(false);
+        }
+
+        private void FillUsersList()
+        {
+            tvuserslist.Text = "Users List:";
+            foreach (var user in users)
+            {
+                tvuserslist.Text += $"\n{user.FirstName} {user.UserEmail}";
+            }
+        }
+
+        private void ShowProgressBar(bool show)
+        {
+            //android:background="@android:color/transparent"
+
+            if (show)
+            {
+                mProgressDialog = new Dialog(this, Android.Resource.Style.ThemeNoTitleBar);
+                View view = LayoutInflater.From(this).Inflate(Resource.Layout.fb_progressbar, null);
+                //var mProgressMessage = (TextView)view.FindViewById(Resource.Id.;
+                //mProgressMessage.Text = "Loading...";
+                mProgressDialog.Window.SetBackgroundDrawableResource(Resource.Color.mtrl_btn_transparent_bg_color);
+                mProgressDialog.SetContentView(view);
+                mProgressDialog.SetCancelable(false);
+                mProgressDialog.Show();
+            }
+            else
+            {
+                mProgressDialog.Dismiss();
+            }
+        }
+    }
+}
---
+++ Service/FireBaseHelper.cs
@@ -0,0 +1,285 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Gms.Extensions;
+using Android.Gms.Tasks;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Big17DataFirebase2.BusinessLogic;
+using Big17DataFirebase2.Model;
+using Firebase;
+using Firebase.Auth;
+using Firebase.Firestore;
+using Firebase.Firestore.Auth;
+using Java.Lang;
+using Java.Util;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Xamarin.Grpc.NameResolver;
+
+namespace Big17DataFirebase2.Service
+{
+	public class FireBaseHelper
+	{
+		public static FirestoreEventListener FirestoreEventListener;
+        protected static FireBaseHelper me;
+		private FirebaseApp app;
+		
+		static FireBaseHelper() { me = new FireBaseHelper(); }
+
+		protected FireBaseHelper() { InitializeFirebase(); }
+
+		//Initialize Firebase app
+		private void InitializeFirebase()
+		{
+			try
+			{
+				//1.
+				//Parse Firebase json file:
+				//Install Newtonsoft.Json NuGet latest version
+				//Rename json file google-services.json to googleservices.json 
+				//Place json file google-services.json into Root/Assets
+				//Set its Build Action in Property to "AndroidAsset"	
+
+				string json;
+				string projectId = "";
+				string apiKey = "";
+				string storageBucket = "";
+				AssetManager assets = Application.Context.Assets;
+				using (Stream stream = assets.Open("googleservices.json")) //Correct way to access raw resource
+				{
+					// Reading from app data directory
+					using (StreamReader r = new StreamReader(stream))
+					{
+						json = r.ReadToEnd();
+
+						//using Newtonsoft.Json.Linq;
+						//JObject.Parse(json) parses the JSON string into a JObject, making it easy to navigate the JSON structure.
+						//JToken is used to access the individual elements within the JSON.
+						JObject jsonObj = JObject.Parse(json);
+						JToken projectInfo = jsonObj["project_info"];
+
+						if (projectInfo != null)
+						{
+							projectId = (string)projectInfo["project_id"];
+							storageBucket = (string)projectInfo["storage_bucket"];
+						}
+						else
+						{
+							Log.Error(ProManager.TAG, "project_info is null");
+							return; //Exit, as we cannot continue without project_info
+						}
+
+						JToken client = jsonObj["client"][0]; // Access the client array
+						apiKey = (string)client["api_key"][0]["current_key"];
+					}
+				}
+
+				//2. Initilize Firebase App
+				app = FirebaseApp.InitializeApp(Application.Context); //using Firebase;
+				if (app == null)
+				{
+					var options = new FirebaseOptions.Builder()
+					.SetProjectId(projectId)
+					.SetApplicationId(projectId)
+					.SetApiKey(apiKey)
+					.SetDatabaseUrl(projectId + ".firebaseapp.com")
+					.SetStorageBucket(storageBucket)
+					.Build();
+
+					app = FirebaseApp.InitializeApp(Application.Context, options);
+				}
+			}
+			catch (FileNotFoundException ex)
+			{
+				Android.Util.Log.Error(ProManager.TAG, $"File not found: {ex.Message}");
+			}
+			catch (System.Exception ex)
+			{
+				Android.Util.Log.Error(ProManager.TAG, $"Error parsing JSON: {ex.Message}");
+			}
+		}
+
+		#region Users
+		public static async Task<string> SignInUserAsync(string uemail, string upass)
+		{
+			try
+			{
+				FirebaseAuth mAuth = FirebaseAuth.Instance;
+				//using Android.Gms.Extensions;
+				await mAuth.SignInWithEmailAndPassword(uemail, upass);
+				Log.Debug(ProManager.TAG, $"MyApp: User Auth {uemail} SignIn success");
+				return mAuth.CurrentUser.Uid; // Indicate success
+			}
+			catch (FirebaseAuthException ex)
+			{
+				Log.Error(ProManager.TAG, $"SignInUserAsync: User Auth SignIn failed: {ex.Message}");
+				return null; // Indicate failure
+			}
+			catch (System.Exception ex)
+			{
+				Log.Error(ProManager.TAG, $"SignInUserAsync: User Auth SignIn failed, general error: {ex.Message}");
+				return null; // Indicate failure
+			}
+		}
+		public static async Task<string> RegisterUserForAuth(Model.User user)
+		{
+            try
+            {
+                FirebaseAuth mAuth = FirebaseAuth.Instance;
+                //using Android.Gms.Extensions;
+                await mAuth.CreateUserWithEmailAndPasswordAsync(user.UserEmail, user.UserPass);
+                Log.Debug(ProManager.TAG, $"RegisterUserForAuth: User Auth {user.UserEmail} SignIn success");
+             
+				return mAuth?.CurrentUser.Uid;
+            }
+            catch (FirebaseAuthException ex)
+            {
+                Log.Error(ProManager.TAG, $"SignInUserAsync: User Auth SignIn failed: {ex.Message}");
+                return null; // Indicate failure
+            }
+            catch (System.Exception ex)
+            {
+                Log.Error(ProManager.TAG, $"SignInUserAsync: User Auth SignIn failed, general error: {ex.Message}");
+                return null; // Indicate failure
+            }           
+        }
+        public static async Task<bool> InsertAsync(Model.User user)
+        {
+            try
+            {
+                //Insert user to FireStore database
+                HashMap userMap = new HashMap(); //using Java.Util;
+                userMap.Put("FirstName", user.FirstName);
+                userMap.Put("IsAdmin", user.IsAdmin);
+                userMap.Put("LastName", user.LastName);
+                userMap.Put("UserEmail", user.UserEmail);
+                userMap.Put("UserMobile", user.UserMobile);
+                userMap.Put("UserPassword", user.UserPass);
+
+
+                DocumentReference userReference = FirebaseFirestore.Instance
+                                                                        .Collection("users")
+                                                                        .Document(user.Id);
+                await userReference.Set(userMap);
+                Log.Debug(ProManager.TAG, $"InsertAsync: Insert User to Firestore complited");
+                return true; // Indicate success
+            }
+            catch (FirebaseFirestoreException ex)
+            {
+                Log.Error(ProManager.TAG, $"InsertAsync: Insert User to Firestore failed: {ex.Message}");
+                return false; // Indicate failure
+            }
+            catch (System.Exception ex)
+            {
+                Log.Error(ProManager.TAG, $"MyApp: Insert User to Firestore failed: {ex.Message}");
+                return false; // Indicate failure
+            }
+        }
+		public static async Task<Model.User> GetUserById(string userId)
+		{
+			Model.User newuser = null;
+			try
+			{
+                DocumentReference userRef = FirebaseFirestore.Instance
+                .Collection("users")
+                .Document(userId);
+
+                var userObject = await userRef.Get();
+				
+				newuser = new Model.User()
+				{
+					Id = userId,
+					FirstName = ((DocumentSnapshot)userObject).Get("FirstName").ToString(),
+					LastName = ((DocumentSnapshot)userObject).Get("LastName").ToString(),
+					UserEmail = ((DocumentSnapshot)userObject).Get("UserEmail").ToString(),
+					UserMobile = ((DocumentSnapshot)userObject).Get("UserMobile").ToString(),
+					UserPass = ((DocumentSnapshot)userObject).Get("UserPassword").ToString(),
+					IsAdmin = bool.Parse(((DocumentSnapshot)userObject).Get("IsAdmin").ToString())
+				};
+                Log.Debug(ProManager.TAG, $"GetUserById: Get User from Firestore DB success");
+                return newuser;
+            }
+			catch (FirebaseFirestoreException ex)
+			{
+                Log.Debug(ProManager.TAG, $"GetUserByID: Get User from Firestore failed: {ex.Message}");
+                return null; // Indicate failure
+            }
+			catch (System.Exception ex)
+			{
+                Log.Debug(ProManager.TAG, $"GetUserByID general error: {ex.Message}");
+                return null;
+			}
+        }
+        public static async Task<List<Model.User>> GetUsersCollection()
+        {
+			List <Model.User> users = new List <Model.User>();
+
+			try
+			{
+                var documents = await FirebaseFirestore.Instance.Collection("users").Get();
+				var FirestoreUsersCollection = (QuerySnapshot)documents;
+
+                if (!FirestoreUsersCollection.IsEmpty)
+                {
+                    var usersCollection = FirestoreUsersCollection.Documents;
+                    foreach (DocumentSnapshot item in usersCollection)
+                    {
+                        Model.User user = new Model.User()
+                        {
+                            Id = item.Id,
+                            FirstName = item.Get("FirstName").ToString(),
+                            LastName = item.Get("LastName").ToString(),
+                            UserEmail = item.Get("UserEmail").ToString(),
+                            UserMobile = item.Get("UserMobile").ToString(),
+                            UserPass = item.Get("UserPassword").ToString(),
+                            IsAdmin = bool.Parse(item.Get("IsAdmin").ToString())
+                        }; 
+						users.Add(user);
+                    }
+					Log.Debug(ProManager.TAG, $"GetUsersCollection: loaded successfully! " +
+											  $"Count: {users.Count}");                   
+                }
+                return users;
+            }
+            catch (FirebaseFirestoreException ex)
+            {
+                Log.Debug(ProManager.TAG, $"GetUsersCollection failed: {ex.Message}");
+                return users; // Indicate failure
+            }
+            catch (System.Exception ex)
+            {
+                Log.Debug(ProManager.TAG, $"GetUsersCollection general error: {ex.Message}");
+                return users;
+            }
+        }
+        public static void FetchUsersListener()
+        {
+            FirestoreEventListener = new FirestoreEventListener();
+            FirebaseFirestore.Instance
+                .Collection("users")
+                .AddSnapshotListener(FirestoreEventListener);
+        }     
+        #endregion
+    }
+    public class FirestoreEventListener : Java.Lang.Object, Firebase.Firestore.IEventListener
+    {
+        public event EventHandler<TaskListenerEventArgs> getEvent;
+        public class TaskListenerEventArgs : EventArgs
+        {
+            public Java.Lang.Object Result { get; set; }
+        }
+        public void OnEvent(Java.Lang.Object obj, FirebaseFirestoreException error)
+        {
+            getEvent?.Invoke(this, new TaskListenerEventArgs { Result = obj });
+        }
+    }
+}
